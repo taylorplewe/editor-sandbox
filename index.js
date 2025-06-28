36c6342302289cb993956b5a00797c8d8cb80f36
Taylor Plewe <taylor.plewe@awardco.com>
2025-06-27 19:16:26 -0600

    add type=module

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var xterm_1 = require("@xterm/xterm");
-var term = new xterm_1.Terminal({
+const xterm_1 = require("@xterm/xterm");
+const term = new xterm_1.Terminal({
     disableStdin: false,
 });
 term.open(document.querySelector("#terminal"));
